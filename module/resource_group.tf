e519b260b2618191e874708b2822ece2b1b15f9c
pooja <151918pooja@gmail.com>
2025-08-12 21:16:33 +0530

    space added

--- module/resource_group.tf
+++ module/resource_group.tf
@@ -3,12 +3,9 @@ resource "azurerm_resource_group" "rishna-rg1" {
   location = "West Europe"
 }
 
+
+
 resource "azurerm_resource_group" "rishna-rg123" {
   name     = "rishna-rg123"
   location = "West Europe"
 }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> a151b30f5351c4eeb21a6ca61657aaed59be8ac9
